f684d0fa0d372b8fd09ae6330f9359ea561a68d1
John D Pell <John+git@gaelicWizard.net>
2021-08-01 23:07:43 -0700

    variables

--- logr.bash
+++ logr.bash
@@ -12,49 +12,53 @@
 # logr [log|notice|info|debug|warn|error] MESSAGE
 # # or default to "user.info" facility
 # logr MESSAGE
-__logr_DEFAULT_LOG_DIR="${HOME}/logs"
-__logr_DEFAULT_LOG="scripts"
+declare __logr_DEFAULT_LOG="scripts"
 
-unset __logr_LOG_NAME
-unset __logr_SCRIPT_LOG
-unset __logr_VERBOSE
+declare __logr_LOG_NAME=
+declare __logr_SCRIPT_LOG=
+declare __logr_VERBOSE=
 logr() {
-	[[ -z $__logr_LOG_DIR ]] && __logr_LOG_DIR=$__logr_DEFAULT_LOG_DIR
+	: ${__logr_LOG_DIR:="${HOME}/logs"}
 	# default to "user" facility, can be set to local[0-9], etc.
-	[[ -z $__logr_FACILITY ]] && __logr_FACILITY=user
+	: ${__logr_FACILITY:=user}
 	# default to quiet, no output to STDERR
-	[[ -z $__logr_VERBOSE ]] && __logr_VERBOSE=false
+	: ${__logr_VERBOSE:=false}
 	# default log tag and filename to "scripts", changed via logr start command
-	[[ -z "${__logr_LOG_NAME}" ]] && __logr_LOG_NAME=$__logr_DEFAULT_LOG
-	[[ -z "${__logr_SCRIPT_LOG}" ]] && __logr_SCRIPT_LOG="${__logr_LOG_DIR%/}/${__logr_LOG_NAME}.log"
+	: ${__logr_LOG_NAME:=$__logr_DEFAULT_LOG}
+	: ${__logr_SCRIPT_LOG:="${__logr_LOG_DIR%/}/${__logr_LOG_NAME}.log"}
 
-	local function_name="${FUNCNAME[1]}"
-	local log_type=$1
-
+	local function_name="${FUNCNAME[1]:-${BASH_SOURCE[1]:-}}"
+	local log_type
+	
+	if [[ ${1:-info} =~ ^(notice|log|info|warn(ing)?|err(or)?|emerg) ]]
+	then
+		log_type=${1:-info}
+		[[ "${1:-}" ]] && shift
+	fi
 
 	# start must be called first, initializes logging, sets global log file
 	# param 1: (string, optional) [verbose|quiet], verbose echos to STDERR, defaults to quiet
 	# param 2: (string, optional) name of log source, defaults to "scripts" (.log will be appended)
 	if [[ $log_type == "start" ]]; then
 		local should_clean=false
-		mkdir -p "${HOME}/logs/"
-		if [[ $2 =~ (^-v$|^verbose$) ]]; then
+		mkdir -p "${__logr_LOG_DIR:=$__logr_DEFAULT_LOG_DIR}"
+		if [[ $1 =~ (^-v$|^verbose$) ]]; then
 			__logr_VERBOSE=true
 			shift
-		elif [[ $2 =~ (^-q$|^quiet$) ]]; then
+		elif [[ $1 =~ (^-q$|^quiet$) ]]; then
 			__logr_VERBOSE=false
 			shift
 		else
 			__logr_VERBOSE=false
 		fi
 
-		if [[ $2 =~ clea[nr] ]]; then
+		if [[ $1 =~ clea[nr] ]]; then
 			should_clean=true
 			shift
 		fi
 
-		if [[ -n "$2" ]]; then
-			__logr_LOG_NAME=$2
+		if [[ -n "$1" ]]; then
+			__logr_LOG_NAME=$1
 		fi
 
 		__logr_SCRIPT_LOG="${HOME}/logs/${__logr_LOG_NAME}.log"
@@ -76,7 +80,7 @@ logr() {
 		__logr_exec debug "${__logr_LOG_NAME}:${function_name}" "${*:2}"
 	# log, notice, info, warn, error set logging level
 	# warn and error go to /var/log/system.log as well as logfile
-	elif [[ $log_type =~ ^(notice|log|info|warn(ing)?|err(or)?|emerg) ]]; then
+	elif [[ ${log_type:=info} =~ ^(notice|log|info|warn(ing)?|err(or)?|emerg) ]]; then
 		local level
 		case $log_type in
 			notice|log) level="notice" ;;
@@ -100,8 +104,8 @@ logr() {
 __logr_exec() {
 	local cmd
 	if [[ $__logr_VERBOSE == true ]]; then
-		logger -p ${__logr_FACILITY}.$1 -t $2 -s $3 2>&1 | tee -a ${__logr_SCRIPT_LOG} 1>&2
+		logger -p ${__logr_FACILITY}.$1 -t $1 -s $2 2>&1 | tee -a ${__logr_SCRIPT_LOG} 1>&2
 	else
-		logger -p ${__logr_FACILITY}.$1 -t $2 -s $3 2>> ${__logr_SCRIPT_LOG}
+		logger -p ${__logr_FACILITY}.$1 -t $1 -s $2 2>> ${__logr_SCRIPT_LOG}
 	fi
 }
