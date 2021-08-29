02a6b334fbe5f3fee4299e1eeece8fbd521cf4d8
John D Pell <John+git@gaelicWizard.net>
2021-08-28 18:15:59 -0700

    Switch on arguments, rather than `if` ladder
    Convert `if` ladder to `case` statement

--- logr.bash
+++ logr.bash
@@ -11,16 +11,19 @@
 # # or default to "user.info" facility
 # logr MESSAGE
 
-logr()
+function logr()
 {
+	local verb="$1"
+	local caller_name="${FUNCNAME[1]}"
+	# `$FUNCNAME` will reflect 'main' if the caller is the script itself, or 'source' if the caller is a sourced script.
+
 	: "${__logr_LOG_DIR:=${__logr_DEFAULT_LOG_DIR:="${HOME}/Library/Logs"}}"
+	[[ -d "$__logr_LOG_DIR" ]] || mkdir -p "$__logr_LOG_DIR"
+
 	# default log tag and filename to "scripts", changed via logr start command
 	: "${__logr_LOG_NAME:=${__logr_DEFAULT_LOG:="scripts"}}"
 	: "${__logr_SCRIPT_LOG:="${__logr_LOG_DIR%/}/${__logr_LOG_NAME}.log"}"
 
-	local verb="$1"
-
-	local caller_name="${FUNCNAME[1]}"
 	case "${caller_name}" in
 	'source')
 		caller_name="${BASH_SOURCE[1]##*/}"
@@ -28,13 +31,11 @@ logr()
 		;;
 	esac
 
-	[[ -d "$__logr_LOG_DIR" ]] || mkdir -p "$__logr_LOG_DIR"
-
 	# start must be called first, initializes logging, sets global log file
 	# param 1: (string, optional) [verbose|quiet], verbose echos to STDERR, defaults to quiet
 	# param 2: (string, optional) name of log source, defaults to "scripts" (.log will be appended)
-	if [[ $verb == "start" ]]
-	then
+	case "$verb" in
+	'start')
 		local should_clean=false
 		if [[ $2 =~ (^-v$|^verbose$) ]]
 		then
@@ -59,49 +60,66 @@ logr()
 
 		__logr_SCRIPT_LOG="${__logr_LOG_DIR}/${__logr_LOG_NAME}.log"
 		$should_clean && logr clear
-		__logr_exec info "$__logr_LOG_NAME" "====> BEGIN LOGGING"
+		__logr_logger info "$__logr_LOG_NAME" "====> BEGIN LOGGING"
+		;;
+	'quiet')
 	# logr quiet => disables STDERR output
-	elif [[ $verb == "quiet" ]]
-	then
 		__logr_VERBOSE=false
+		;;
+	'verbose')
 	# logr verbose => enables STDERR output
-	elif [[ $verb == "verbose" ]]
-	then
 		__logr_VERBOSE=true
+		;;
+	'clear')
 	# logr clear => clears the log (unless it's the default log)
-	elif [[ $verb == "clear" && $__logr_LOG_NAME != $__logr_DEFAULT_LOG ]]
-	then
-		[[ -n $__logr_SCRIPT_LOG && -f $__logr_SCRIPT_LOG ]] && : > "$__logr_SCRIPT_LOG"
+		if [[ $__logr_LOG_NAME != $__logr_DEFAULT_LOG && -f $__logr_SCRIPT_LOG ]]
+		then
+			: > "$__logr_SCRIPT_LOG"
+		fi
+		;;
+	'debug')
 	# debug type shows full function stack
-	elif [[ $verb == "debug" ]]
-	then
 		caller_name="$(IFS="\\"; echo "${FUNCNAME[*]:1}")"
-		__logr_exec debug "${__logr_LOG_NAME}:${caller_name}" "${*:2}"
+		__logr_logger debug "${__logr_LOG_NAME}:${caller_name}" "${*:2}"
+		;;
+	*)
 	# log, notice, info, warn, error set logging level
 	# warn and error go to /var/log/system.log as well as logfile
-	elif [[ $verb =~ ^(notice|log|info|warn(ing)?|err(or)?|emerg) ]]
-	then
 		local level
 		case $verb in
-			notice|log) level="notice" ;;
-			info) level="info" ;;
-			warn*) level="warning" ;;
-			err*) level="err" ;;
-			emerg) level="emerg" ;;
-			*) level="info" ;;
+			[Dd][Ee][Bb][Uu][Gg])
+				level="debug"
+				caller_name="$(IFS="\\"; echo "${FUNCNAME[*]:1}")"
+				;;
+			[Nn][Oo][Tt][Ii][Cc][Ee]|[Ll][Oo][Gg])
+				level="notice"
+				;;
+			[Ii][Nn][Ff][Oo])
+				level="info"
+				;;
+			[Ww][Aa][Rr][Nn]*)
+				level="warning"
+				;;
+			[Ee][Rr][Rr]*)
+				level="err"
+				;;
+			[Ee][Mm][Ee][Rr][Gg]|[Ff][Aa][Tt][Aa][Ll])
+				level="emerg"
+				;;
+			*)
+				level="info"
+				;;
 		esac
-		__logr_exec $level "${__logr_LOG_NAME}:${caller_name}" "${*:2}"
-	# if no type is given, assumes info level
-	else
-		__logr_exec info "${__logr_LOG_NAME}:${caller_name}" "${*:1}"
-	fi
+		__logr_logger "${level:-info}" "${__logr_LOG_NAME}:${caller_name}" "${*:2}"
+		;;
+	esac
 }
 
 # execute the logger command
 # param 1: (string) [log|notice|info|debug|warn|error] log level
 # param 2: (string) Tag
 # param 3: (string) Message
-__logr_exec()
+function __logr_logger()
 {
 	local level="${1:-info}" tag="${2:-}" message="${3:-}"
 	if [[ ${__logr_VERBOSE:-false} == true ]]
